8eef25ad83cb407d3bd79ba1bfdbe79cc352a779
irunislaw <84100482+irunislaw@users.noreply.github.com>
2023-03-18 17:23:51 +0100

    chcialem zrobic jakies fajne tego menu ale nie umiiem w animacje

--- react-frontend/src/components/Header/Header.tsx
+++ react-frontend/src/components/Header/Header.tsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Transition } from '@tailwindui/react'
 export default function Header() {
-    return (
+    const [activeMenu, setActiveMenu] = useState(false);
+
+    return (<div>
+        <Transition
+        show={activeMenu==true}
+        enter = "transition ease-in-out duration-300 "
+        enterFrom="-top-full"
+        enterTo="top-0"
+        leave=" ease-in-out duration-300"
+        leaveFrom="top-0"
+        leaveTo="-top-full"
+        >
+            <div className="menu h-full  w-full  fixed flex text-6xl flex-col  text-[#FFCD01] ">
+                <div className="flex w-full justify-end p-10">
+                    <button className="flex" onClick={()=>setActiveMenu(!activeMenu)}>x</button>
+                </div>
+                <div className=" h-full justify-between flex flex-col items-center px-24 py-12">
+                    <Link to="">dfsdfsdfsie</Link>
+                    <Link to="">cos ta fdsfs</Link>
+                    <Link to="/login">login</Link>
+                </div>
+            </div>
+        </Transition>
         <div className=" bg-[#181B30] items-center flex w-full px-32 py-16 flex-row justify-center lg:justify-between  ">
             <Link to="/" className="text-5xl font-semibold">
                 <span className="text-[#EF2A4F]">Poker</span>
@@ -11,7 +35,7 @@ export default function Header() {
                 <Link to="">cos ta fdsfs</Link>
                 <Link to="/login">login</Link>
             </div>
-            <button className="flex lg:hidden border-none p-1 ">
+            <button className="flex lg:hidden border-none p-1 " onClick={() => {setActiveMenu(!activeMenu); console.log("siema"+activeMenu)}}>
                 <span className="Button-content">
                     <span className="Button-label">
                         <div className="HeaderMenu-toggle-bar rounded-md my-1"></div>
@@ -22,5 +46,6 @@ export default function Header() {
             </button>
             {/* TODO profile handling and login/logout */}
         </div>
+    </div>
     );
 }

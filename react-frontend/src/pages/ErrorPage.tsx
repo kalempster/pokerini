e2ad64d4bdb82eae34520da3a5b9ea51dd99c88c
irunislaw <michalirek0505@gmail.com>
2023-03-28 11:16:07 +0200

    added aos to 404 page

--- react-frontend/src/pages/ErrorPage.tsx
+++ react-frontend/src/pages/ErrorPage.tsx
@@ -5,8 +5,8 @@ const ErrorPage =()=>{
     return <>
     <Section isFirst isSingle className="flex items-center justify-center ">
         <div className="flex gap-5 w-3/4 lg:w-2/3 flex-col lg:flex-row">
-            <ReactSVG src={error} className="w-3/4"></ReactSVG>
-            <div className="flex text-white font-semibold text-3xl md:text-4xl lg:text-5xl xl:text-6xl lg:h-full lg:items-center lg:justify-center">That’s not what you
+            <ReactSVG   data-aos="fade-up" src={error} className="w-3/4"></ReactSVG>
+            <div   data-aos="fade-up" className="flex text-white font-semibold text-3xl md:text-4xl lg:text-5xl xl:text-6xl lg:h-full lg:items-center lg:justify-center">That’s not what you
 are looking for!</div>
         </div>
     </Section>

4bdaa17209e32c4d585cdb93467ec2cd9b5a7c23
kalempster <mrpiter1337@gmail.com>
2023-05-02 20:38:54 +0200

    Added eslint and prettier to backend zeby irek mi kurwa nie probowal znowu dawac 2 spacje jako tab do kurwy

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,11 @@
-import { TRPCError, inferAsyncReturnType, initTRPC } from "@trpc/server";
 import { PrismaClient } from "@prisma/client";
-import * as express from "express";
+import { inferAsyncReturnType, initTRPC } from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { config } from "dotenv";
+import * as express from "express";
+import { ZodError, z } from "zod";
+import { authRouter } from "./routers/auth";
 import { envSchema } from "./zod/env";
-import { TypeOf, ZodError, z } from "zod";
-
 
 export const prisma = new PrismaClient();
 
@@ -14,49 +14,47 @@ const envs = config();
 export let env: z.infer<typeof envSchema>;
 
 try {
-  env = envSchema.parse(envs.parsed);
+    env = envSchema.parse(envs.parsed);
 } catch (error) {
-  if (error instanceof ZodError) {
-    for (const err of error.errors) {
-      console.log(`Env missing: ${err.path}`);
+    if (error instanceof ZodError) {
+        for (const err of error.errors) {
+            console.log(`Env missing: ${err.path}`);
+        }
     }
-  }
-  process.exit(1);
+    process.exit(1);
 }
 
 const app = express();
 
-
 const createContext = ({
-  req,
-  res,
+    req,
+    res
 }: trpcExpress.CreateExpressContextOptions) => ({ req, res });
 type Context = inferAsyncReturnType<typeof createContext>;
 export const t = initTRPC.context<Context>().create({
-  errorFormatter(opts) {
-    const { shape, error } = opts;
-    return {
-      ...shape,
-      data: {
-        ...shape.data,
-        zodError:
-          error.code === "BAD_REQUEST" && error.cause instanceof ZodError
-            ? error.cause.flatten()
-            : null,
-      },
-    };
-  },
-  
+    errorFormatter(opts) {
+        const { shape, error } = opts;
+        return {
+            ...shape,
+            data: {
+                ...shape.data,
+                zodError:
+                    error.code === "BAD_REQUEST" &&
+                    error.cause instanceof ZodError
+                        ? error.cause.flatten()
+                        : null
+            }
+        };
+    }
 });
-import { authRouter } from "./routers/auth";
 export const appRouter = t.router({
-    auth: authRouter,
+    auth: authRouter
 });
-export const publicProcedure = t.procedure
+export const publicProcedure = t.procedure;
 app.use(
     "/api",
     trpcExpress.createExpressMiddleware({
         router: appRouter,
-        createContext,
+        createContext
     })
 );

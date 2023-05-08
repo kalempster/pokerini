fc8630cfbde962a65ec9f22fcf82ff8e33039417
irunislaw <84100482+irunislaw@users.noreply.github.com>
2023-05-08 20:17:37 +0200

    implemented whole poker logic

--- react-frontend/src/index.tsx
+++ react-frontend/src/index.tsx
@@ -6,7 +6,9 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 {
